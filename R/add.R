26b378682445b7ab488aa9966d15c0f90f0d8f57
Xiao Bai <au734914@uni.au.dk>
2024-08-22 11:30:30 +0200

    FIrst

---
+++ R/add.R
@@ -0,0 +1,20 @@
+#' Title
+#'
+#' This will be the description.
+#' 
+#' These will be the details.
+#'
+#'
+#' @param x 
+#' @param y 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' my_fancy_add(1,2)
+#' my_fancy_add(1:2, 2:3)
+my_function_add <- function(x, y){
+  x + y
+}
+
---
+++ R/select2.R
@@ -0,0 +1,12 @@
+#' Title
+#'
+#' @param df 
+#' @param vars 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+select2 <- function(df, vars){
+  df[, vars]
+}
